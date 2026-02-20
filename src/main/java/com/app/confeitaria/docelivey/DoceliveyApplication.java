4174f76356c3928c2a3ca9aa4c5b7f0edc2ac846
Beatriz <biaeadao123@gmail.com>
2026-02-20 20:08:19 -0300

    Projeto confeitaria aula professor Rogerio

---
+++ src/main/java/com/app/confeitaria/docelivey/DoceliveyApplication.java
@@ -0,0 +1,13 @@
+package com.app.confeitaria.docelivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoceliveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoceliveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/confeitaria/docelivey/DoceliveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.confeitaria.docelivey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoceliveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
